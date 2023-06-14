9a97d4c08e6b9eb2eddb1327e9a99c569fc13d8f
Nasim Khalili <nasimkhalili1385nasim@gmail.com>
2023-06-14 17:08:56 +0330

    Counter II

---
+++ CounterII.js
@@ -0,0 +1,16 @@
+var createCounter = function (init) {
+  let num = init;
+  return {
+    increment: () => ++num,
+    decrement: () => --num,
+    reset: () => (num = init),
+  };
+};
+
+const counter = createCounter(5);
+console.log(counter);
+let ans = [];
+ans.push(counter.increment()); // 6
+ans.push(counter.reset()); // 5
+ans.push(counter.decrement()); // 4
+console.log(ans);
